33f2d40fce16ce9c8aa9a5ea23f31f3ab57e6aea
Ameya <ameyapatil2424@gmail.com>
2024-09-25 06:17:16 -0700

    Default BG setup

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,13 @@
+import React from 'react'
 import {Box} from '@chakra-ui/react'
+import CodeEditor from './components/CodeEditor'
 
 function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <Box>CODE FLOW!</Box>
+   
+  return <Box
+            minH="100vh" bg='#0f0a19' color='grey.500' px={6} py={6}>
+              <CodeEditor />
+          </Box>
 }
 
 export default App
